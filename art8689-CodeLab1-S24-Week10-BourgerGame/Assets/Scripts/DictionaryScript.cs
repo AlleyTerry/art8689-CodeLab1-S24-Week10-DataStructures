cce2a54fcbfbe0016e1b6419a9f10fc8f06c20c4
AlleyTerry <art8689@nyu.edu>
2024-04-05 19:06:21 -0400

    borger finished

--- art8689-CodeLab1-S24-Week10-BourgerGame/Assets/Scripts/DictionaryScript.cs
+++ art8689-CodeLab1-S24-Week10-BourgerGame/Assets/Scripts/DictionaryScript.cs
@@ -2,24 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.Mathematics;
 using UnityEngine;
+using Unity.UI;
+using TMPro;
 
 public class DictionaryScript : MonoBehaviour
 {
     public Dictionary<string, int> ingredients = new Dictionary<string, int>();
     public Dictionary<string, int> customerburger1 = new Dictionary<string, int>();
+    public TextMeshProUGUI order;
+    
 
     public GameObject bun;
     private int bunNumber;
 
     public GameObject lettuce;
-
+    private int lettuceNumber;
+    
     public GameObject meat;
+    private int meatNumber;
 
     public GameObject cheese;
+    private int cheeseNumber;
     // Start is called before the first frame update
     void Start()
     {
-        customerburger1.Add("bun",4);
+        customerburger1.Add("bun",2);
+        customerburger1.Add("meat",2);
+        customerburger1.Add("cheese",2);
+        customerburger1.Add("lettuce",2);
     }
 
     // Update is called once per frame
@@ -45,19 +55,71 @@ public class DictionaryScript : MonoBehaviour
         bunNumber = ingredients["bun"];
         Debug.Log(bunNumber);
     }
+    
+        public void MeatAdd()
+        {
+            if (ingredients.ContainsKey("meat"))
+            {
+                Instantiate(meat, new Vector3(0,2.0f,0), quaternion.identity);
+                ingredients["meat"]++;
+            }
+            else
+            {
+                Instantiate(meat, new Vector3(0,2.0f,0), quaternion.identity);
+                ingredients.Add("meat", 1);
+            }
+    
+            meatNumber = ingredients["meat"];
+            Debug.Log(meatNumber);
+        }
+        public void LettuceAdd()
+        {
+            if (ingredients.ContainsKey("lettuce"))
+            {
+                Instantiate(lettuce, new Vector3(0,2.0f,0), quaternion.identity);
+                ingredients["lettuce"]++;
+            }
+            else
+            {
+                Instantiate(lettuce, new Vector3(0,2.0f,0), quaternion.identity);
+                ingredients.Add("lettuce", 1);
+            }
+    
+            lettuceNumber = ingredients["lettuce"];
+            Debug.Log(lettuceNumber);
+        }
+        public void CheeseAdd()
+        {
+            if (ingredients.ContainsKey("cheese"))
+            {
+                Instantiate(cheese, new Vector3(0,2.0f,0), quaternion.identity);
+                ingredients["cheese"]++;
+            }
+            else
+            {
+                Instantiate(cheese, new Vector3(0,2.0f,0), quaternion.identity);
+                ingredients.Add("cheese", 1);
+            }
+    
+            cheeseNumber = ingredients["cheese"];
+            Debug.Log(cheeseNumber);
+        }
+        
 
     public void MakeBurger()
     {
         Debug.Log(ingredients["bun"]);
         Debug.Log(customerburger1["bun"]);
         //check to see if burger matches customer burger
-        if (ingredients["bun"] == customerburger1["bun"])
+        if (ingredients["bun"] == customerburger1["bun"] && ingredients["meat"] == customerburger1["meat"]
+                                                         && ingredients["cheese"] == customerburger1["cheese"] 
+                                                         && ingredients["lettuce"] == customerburger1["lettuce"])
         {
-            Debug.Log("yayy go to sleep");
+            order.text = "yes my order yipeeee";
         }
         else
         {
-            Debug.Log("not equal?");
+            order.text = "I want to speak to the manager";
         }
         
     }
